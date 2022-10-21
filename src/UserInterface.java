c73e955ed28648d5ebce0df9a3d950bf55686152
willaaa <willaaa@umich.edu>
2022-10-20 21:42:01 -0400

    clean up code

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -6,7 +6,6 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowAdapter;
@@ -16,13 +15,11 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class UserInterface extends JPanel implements MouseListener {
-    static String previous = "000000";
     BlankArea blankArea;
     JFrame myFrame = new JFrame("Black Box");
     private ScheduledExecutorService executorService;
     static ArrayList<String> clickTime = new ArrayList<String>();
     static ArrayList<String> metTime = new ArrayList<String>();
-
 
 
     public void runner() {
@@ -43,7 +40,7 @@ public class UserInterface extends JPanel implements MouseListener {
                     startButton.removeActionListener(this);
                     myFrame.remove(startButton);
                     JComponent newContentPane = new UserInterface();
-                    newContentPane.setOpaque(true); //content panes must be opaque
+                    newContentPane.setOpaque(true);
                     myFrame.setContentPane(newContentPane);
                     myFrame.pack();
                     myFrame.repaint();
@@ -51,15 +48,6 @@ public class UserInterface extends JPanel implements MouseListener {
 
                     executorService.schedule(() -> {
                         myFrame.dispatchEvent(new WindowEvent(myFrame, WindowEvent.WINDOW_CLOSING));
-//                        try {
-//                            writeToCSV();
-//                            System.exit(0);
-//                        } catch (FileNotFoundException ex) {
-//                            throw new RuntimeException(ex);
-//                        }
-
-
-                        //}, 1, TimeUnit.MINUTES);
                     }, 15, TimeUnit.SECONDS);
 
 
@@ -68,8 +56,6 @@ public class UserInterface extends JPanel implements MouseListener {
                 }
 
             }
-
-
         });
 
         myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -84,23 +70,6 @@ public class UserInterface extends JPanel implements MouseListener {
                 }
             }
         });
-//        myFrame.addWindowListener(new WindowAdapter() {
-//            @Override
-//            public void windowClosing(WindowEvent event) {
-//                myFrame.dispose();
-//                try {
-//                    CleanData.sort();
-//                } catch (FileNotFoundException fnfe) {
-//                    fnfe.printStackTrace();
-//                }
-//                System.out.println("bye bye");
-//                //System.exit(0);
-//
-//
-//            }
-//        });
-
-
     }
 
     public UserInterface() {
@@ -108,64 +77,30 @@ public class UserInterface extends JPanel implements MouseListener {
         blankArea.addMouseListener(this);
         addMouseListener(this);
         setPreferredSize(new Dimension(450, 450));
-
     }
 
     public void mouseClicked(MouseEvent e) {
-
-    }
-
-//    public String timeKeeper(String time, String previous) throws FileNotFoundException {
-//        previous = previous + "\n" + time;
-//        writeFile(previous);
-//        return previous;
-//    }
-
-    public void writeFile(String time) throws FileNotFoundException {
-        PrintWriter outFile = new PrintWriter("logs.txt");
-        outFile.println(time);
-        outFile.close();
-
     }
 
     public void mousePressed(MouseEvent e) {
-//clickTime.add("hello sir");
         long time = System.currentTimeMillis();
         clickTime.add(String.valueOf(time));
         metTime.add(Metronome.getCurrentTime());
-//        String stepOne = String.valueOf(time);
-//        stepOne = stepOne.substring(6);
-        //eventOutput("Mouse pressed at " + time,e);
         System.out.println("Mouse pressed at " + time);
-        System.out.println("last met was " + Metronome.getCurrentTime()+"\n");
-//        try {
-//            previous = timeKeeper(stepOne, previous);
-//        } catch (IOException ex) {
-//            ex.printStackTrace();
-//        }
-//        if (Metronome.getCount() >= Metronome.getBeat()) {
-//            myFrame.dispose();
-//        }
-
-
+        System.out.println("last met was " + Metronome.getCurrentTime() + "\n");
     }
 
     public void mouseReleased(MouseEvent e) {
     }
 
     public void mouseEntered(MouseEvent e) {
-
     }
 
     public void mouseExited(MouseEvent e) {
-
     }
 
     public void writeToCSV() throws FileNotFoundException {
         System.out.println("Reached");
-//        for(int i = 0; i < clickTime.size(); i++){
-//            System.out.println(clickTime.get(i));
-//        }
         PrintWriter outFile = new PrintWriter("logs.csv");
         outFile.println("Click Time, Metronome Time");
         for (int i = 0; i < clickTime.size(); i++) {
